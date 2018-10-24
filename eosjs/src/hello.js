31f31ba1d4b0167e08363e7f61adbac60d7d2397
nishant <nishant.singh@birdeye.com>
2018-10-24 20:30:46 +0530

    delegate bandwidth

--- eosjs/src/hello.js
+++ eosjs/src/hello.js
@@ -24,11 +24,31 @@ const buyRam = ({payer, receiver, tokens}) => ({
     },
 });
 
+const delegateBandwidth = ({from, receiver, cpuTokens, netTokens}) => ({
+        account: 'eosio',
+        name: 'delegatebw',
+        authorization: [{
+            actor: 'eoszenmaster',
+            permission: 'active',
+        }],
+        data: {
+            from,
+            receiver,
+            stake_cpu_quantity: `${cpuTokens || '0.0000'} EOS`,
+            stake_net_quantity: `${netTokens || '0.0000'} EOS`,
+            transfer: from !== receiver,
+        }
+    }
+);
+
 (async () => {
     try {
         const api = new Api({rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder()});
         const result = await api.transact({
-            actions: [buyRam({payer: 'eoszenmaster', receiver: 'eoszenmaster', tokens: '0.5000'})]
+            actions: [
+                buyRam({payer: 'eoszenmaster', receiver: 'eoszenmaster', tokens: '0.5000'}),
+                delegateBandwidth({from: 'eoszenmaster', receiver: 'eoszenmaster', cpuTokens: '1.1000'})
+            ]
         }, {
             blocksBehind: 3,
             expireSeconds: 30,
